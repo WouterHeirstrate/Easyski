4306d5e97453cb0ee343ac4b813a336a4543bd6c
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-06 12:34:22 +0200

    feat(map): add map component to app

--- App.tsx
+++ App.tsx
@@ -1,28 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
-
-import { Header, LearnMoreLinks } from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { Map } from './src/components';
 
 const App = () => {
 	return (
 		<SafeAreaView>
 			<StatusBar />
-			<ScrollView contentInsetAdjustmentBehavior="automatic">
-				<Header />
-				<View>
-					<LearnMoreLinks />
-				</View>
-			</ScrollView>
+			<Map />
 		</SafeAreaView>
 	);
 };
